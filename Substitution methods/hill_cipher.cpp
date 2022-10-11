e2ec72e827732c989d355ad95b7d4f453ca2abc4
classrohan <96218175+classrohan@users.noreply.github.com>
2022-10-11 18:56:37 +0530

    Update hill_cipher.cpp

--- Substitution methods/hill_cipher.cpp
+++ Substitution methods/hill_cipher.cpp
@@ -3,64 +3,75 @@
 using namespace std;
 void toLowerCase(string &plain)
 {
-    int i;
-    for (i = 0; i < plain.length(); i++)
-    {
-        if (plain[i] > 64 && plain[i] < 91)
-            plain[i] += 32;
-    }
+  int i;
+  for (i = 0; i < plain.length(); i++)
+  {
+    if (plain[i] > 64 && plain[i] < 91)
+      plain[i] += 32;
+  }
 }
-string matrixMultiply(vector<vector<int>>keyMatrix,vector<int>b){
-      string encodedChar="";
-        int k=0;
-        int temp;
-     for(int i=0;i<keyMatrix.size();i++){
-        temp=0;
-        for(int j=0;j<keyMatrix[0].size();j++){
-          temp+=(keyMatrix[i][j])*(b[(k++)%b.size()]);
-        }
-         encodedChar.push_back((temp%26)+97);
-     }
-     return encodedChar;
-}
-string hillCipher(string plainText, vector<vector<int>>keyMatrix)
+// multiply matrix with array of characters of plainText and return the string of encoded chars
+string matrixMultiply(vector<vector<int>> keyMatrix, vector<int> b)
 {
-    string cipherText = "";
-    vector<int>temp;
-    for (int i = 0; i <plainText.length(); i+=3)
+  string encodedChar = "";
+  int k = 0;
+  int temp;
+
+  for (int i = 0; i < keyMatrix.size(); i++)
+  {
+    temp = 0;
+    for (int j = 0; j < keyMatrix[0].size(); j++)
     {
-      temp.push_back(plainText[i]-97);
-      temp.push_back(plainText[i+1]-97);
-      temp.push_back(plainText[i+2]-97);
-      cipherText+=matrixMultiply(keyMatrix,temp);
-      temp.clear();    
-         
+      temp += (keyMatrix[i][j]) * (b[(k++) % b.size()]);
     }
-    
-    return cipherText;
+    // converts in char and appends to encodedChar
+    encodedChar.push_back((temp % 26) + 97);
+  }
+  return encodedChar;
+}
+string hillCipher(string plainText, vector<vector<int>> keyMatrix)
+{
+  string cipherText = "";
+  vector<int> temp;
+  // Add the characters of palin text with respect to number of columns of key matrix in temp array and pass it to
+  // matrix multiply
+  for (int i = 0; i < plainText.length(); i += keyMatrix[0].size())
+  {
+
+    for (int j = 0; j < keyMatrix[0].size(); j++)
+    {
+      temp.push_back(plainText[i + j] - 97);
+    }
+    cipherText += matrixMultiply(keyMatrix, temp);
+    temp.clear();
+  }
+
+  return cipherText;
 }
 
 int main()
 {
-    string plainText;
-    int row,col;
-    cout << "Enter plain text" << endl;
-    getline(cin, plainText);
+  string plainText;
+  int row, col;
+  cout << "Enter plain text" << endl;
+  getline(cin, plainText);
 
-    cout<<"Enter the number of rows and columns in your key matrix"<<endl;
-    cin>>row>>col;
-    vector<vector<int>> key(row);
-    cout << "Enter key matrix"<<endl;
-    int temp;
-     for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-          cin>>temp;
-          key[i].push_back(temp);
-        }
-     }
-    toLowerCase(plainText);
-    cout << endl;
-   cout << "Plain text: " << plainText << endl
-         << "Cipher Text: " << hillCipher(plainText, key) << endl;
-    return 0;
+  cout << "Enter the number of rows and columns in your key matrix" << endl;
+  cin >> row >> col;
+  vector<vector<int>> key(row);
+  cout << "Enter key matrix" << endl;
+  int temp;
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      cin >> temp;
+      key[i].push_back(temp);
+    }
+  }
+  toLowerCase(plainText);
+  cout << endl;
+  cout << "Plain text: " << plainText << endl
+       << "Cipher Text: " << hillCipher(plainText, key) << endl;
+  return 0;
 }
